3b1eed01869ad1b091e11f15312f6a7a3fdfc184
Anatoly Ostrovsky <aostrovsky@evolution.com>
2025-11-05 01:16:40 +0200

    Observer directive improvements

--- @types/directive/observe/observe.d.ts
+++ @types/directive/observe/observe.d.ts
@@ -1,9 +1,6 @@
 /**
  * @param {string} source - the name of the attribute to be observed
  * @param {string} prop - the scope property to be updated with attribute value
- * @returns {import("../../interface.ts").Directive}
+ * @returns {ng.Directive}
  */
-export function ngObserveDirective(
-  source: string,
-  prop: string,
-): import("../../interface.ts").Directive;
+export function ngObserveDirective(source: string, prop: string): ng.Directive;
--- src/directive/observe/observe.js
+++ src/directive/observe/observe.js
@@ -3,19 +3,18 @@ import { kebabToCamel } from "../../shared/dom.js";
 /**
  * @param {string} source - the name of the attribute to be observed
  * @param {string} prop - the scope property to be updated with attribute value
- * @returns {import("../../interface.ts").Directive}
+ * @returns {ng.Directive}
  */
 export function ngObserveDirective(source, prop) {
   return {
     restrict: "A",
     compile: () => (scope, element) => {
-      const targetElement = element;
       if (prop === "") {
         prop = source;
       }
       const normalized = kebabToCamel(prop);
       if (!scope[normalized]) {
-        scope[normalized] = targetElement.getAttribute(source);
+        scope[normalized] = element.getAttribute(source);
       }
 
       const observer = new MutationObserver((mutations) => {
@@ -28,7 +27,7 @@ export function ngObserveDirective(source, prop) {
         }
       });
 
-      observer.observe(targetElement, {
+      observer.observe(element, {
         attributes: true,
         attributeFilter: [source],
       });
