71f3628039ee11b53b6f0d06a037505dd0379912
Anatoly Ostrovsky <anatolyostrovsky@gmail.com>
2025-11-28 01:12:03 +0200

    Fix import

--- @types/services/stream/interface.d.ts
+++ @types/services/stream/interface.d.ts
@@ -1,25 +1,18 @@
 export interface StreamConnectionConfig {
   /** Called when the connection opens */
   onOpen?: (event: Event) => void;
-
   /** Called when a message is received */
   onMessage?: (data: any, event: Event) => void;
-
   /** Called when an error occurs */
   onError?: (err: any) => void;
-
   /** Called when a reconnect attempt happens */
   onReconnect?: (attempt: number) => void;
-
   /** Delay between reconnect attempts in milliseconds */
   retryDelay?: number;
-
   /** Maximum number of reconnect attempts */
   maxRetries?: number;
-
   /** Timeout in milliseconds to detect heartbeat inactivity */
   heartbeatTimeout?: number;
-
   /** Function to transform incoming messages */
   transformMessage?: (data: any) => any;
 }
