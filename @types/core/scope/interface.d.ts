c719e54ab9109f3bf7ac060da7728084aae61fe2
Anatoly Ostrovsky <anatolyostrovsky@gmail.com>
2025-12-07 04:18:06 +0200

    Remove unused type

--- @types/core/scope/interface.d.ts
+++ @types/core/scope/interface.d.ts
@@ -1,10 +1,4 @@
 import type { CompiledExpression } from "../parse/interface.ts";
-import { Scope } from "./scope.js";
-export interface AsyncQueueTask {
-  handler: Scope;
-  fn: (...args: any[]) => any;
-  locals: Record<string, any>;
-}
 export type ListenerFn = (newValue?: any, originalTarget?: object) => void;
 export interface Listener {
   originalTarget: any;
