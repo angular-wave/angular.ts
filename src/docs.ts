c07f066268fce5745be1c24899958bfc73a60b01
Anatoly Ostrovsky <anatolyostrovsky@gmail.com>
2026-04-19 18:37:28 +0300

    Version bump 0.25.0

--- src/docs.ts
+++ src/docs.ts
@@ -19,3 +19,37 @@ export * from "./core/parse/parse.ts";
 
 export * from "./filters/filter.ts";
 export * from "./core/filter/filter.ts";
+
+export * from "./router/common/trace.ts";
+export * from "./router/router.ts";
+export * from "./router/state/interface.ts";
+export * from "./router/state/state-object.ts";
+export * from "./router/state/state-registry.ts";
+export * from "./router/state/state-service.ts";
+export * from "./router/state/target-state.ts";
+export * from "./router/template-factory.ts";
+export * from "./router/transition/interface.ts";
+export * from "./router/transition/reject-factory.ts";
+export {
+  defaultTransOpts,
+  TransitionProvider,
+} from "./router/transition/transition-service.ts";
+export * from "./router/transition/transition.ts";
+export {
+  defaultConfig,
+  MatchDetails,
+  MatchResult,
+  MatcherUrlRule,
+  ParamDetails,
+  RegExpRule,
+  StateRule,
+  UrlMatcherCache,
+  UrlMatcherCompileConfig,
+  UrlRule,
+  UrlRuleHandlerFn,
+  UrlRuleMatchFn,
+  UrlRuleType,
+} from "./router/url/interface.ts";
+export * from "./router/url/url-config.ts";
+export * from "./router/url/url-service.ts";
+export * from "./router/view/view.ts";
