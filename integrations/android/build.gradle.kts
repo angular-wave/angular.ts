0b1030061ff329b954e8546add1d91b15bf642d8
Anatoly Ostrovsky <anatolyostrovsky@gmail.com>
2026-09-22 04:02:13 +0300

    Stabilize Android release matrix

--- integrations/android/build.gradle.kts
+++ integrations/android/build.gradle.kts
@@ -13,10 +13,10 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompilationTask
 
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-    id("com.android.application") version "9.4.0" apply false
-    id("com.android.library") version "9.4.0" apply false
-    id("com.android.test") version "9.4.0" apply false
-    id("com.android.legacy-kapt") version "9.4.0" apply false
+    id("com.android.application") version "9.4.1" apply false
+    id("com.android.library") version "9.4.1" apply false
+    id("com.android.test") version "9.4.1" apply false
+    id("com.android.legacy-kapt") version "9.4.1" apply false
     id("org.jetbrains.kotlin.plugin.compose") version "2.4.10" apply false
     id("org.jetbrains.kotlin.plugin.serialization") version "2.4.10" apply false
     id("com.vanniktech.maven.publish") version "0.37.0" apply false
